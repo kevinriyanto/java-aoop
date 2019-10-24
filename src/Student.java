d4292515673a9d3a9a8bc6351c08e99edef0fbb9
kevinriyanto <kevinriyanto223344@gmail.com>
2019-10-24 12:12:38 +0700

    update people

--- src/Student.java
+++ src/Student.java
@@ -1,20 +1,19 @@
-
-
-public class Student extends People implements Runnable{
+public class Student extends People implements Eatable{
 	private Nim nim;
-	public Student(String name, int age) {
-		super(name, age);
-		// TODO Auto-generated constructor stub
+	public Student(String name, int age,Nim nim) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.nim = nim;
 	}
 	public Nim getNim() {
 		return nim;
 	}
-
 	public void setNim(Nim nim) {
 		this.nim = nim;
 	}
 	@Override
-	public void run() {
-		System.out.println("student run");
+	public void eat() {
+		System.out.println("student eat");
 	}
 }
